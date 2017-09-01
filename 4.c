1c24b1bd96880e40c2bb84109ef6370b6e148ec5
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2017-09-01 14:46:42 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char m;
+    printf("Enter a character: ");
+    scanf("%c",&m);
+
+    if('a'||'b'||'c'||'d'||)
+        printf("%c is an alphabet.",c);
+    else
+        printf("%c is not an alphabet.",c);
+
+    return 0;
+}
