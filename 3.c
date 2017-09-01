21c2826865d60eb7dafda86aad93062d7bdbc669
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2017-09-01 14:40:38 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char n;
+   printf("Enter character ");
+    scanf("%c",&n);
+    if(n == 'a' || n == 'e' || n == 'i' || n== 'o' || n == 'u')
+      {
+      printf("%c is a vowel.",n);
+      }
+    else
+    {
+        printf("%c is a conso", n);
+        }
+    return 0;
+}
