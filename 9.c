c7d5d5b5f16e87f58d0915044765eb8af105fb50
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2017-09-04 19:18:21 +0530

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n, count, s= 0;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+
+    for(count=1; count<= n; ++i)
+    {
+        s=s =i ;   
+    }
+
+    printf("S= %d",s);
+
+    return 0;
+}
