a79c8ad028760f651058905a317f1635e467f934
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2017-09-04 19:23:54 +0530

    Create 9 sum.c

---
+++ 9 sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n, count, s= 0;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+
+    for(count=1; count<= n; ++count)
+    {
+        s=s+i ;   
+    }
+
+    printf("S= %d",s);
+
+    return 0;
+}
