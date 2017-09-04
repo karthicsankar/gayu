4aa13769a3147ddab3cd81376c08f5d9b8d95442
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2017-09-04 19:44:48 +0530

    Create 5c.c

---
+++ 5c.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int s1, s2, s3;
+
+    printf("Enter the number");
+    scanf("%d%d%d",&s1,&s2,&s3);
+    if( n1>=n2 && n1>=n3 )
+    {
+        printf("%d is the largest number.", s1);
+        }
+
+    if( n2>=n1 && n2>=n3 )
+    {
+        printf("%d is the largest number.", s2);
+        }
+
+    else
+    {
+        printf("%d is the largest number.", s3);
+        }
