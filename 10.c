57c2e4490a19d88847a0135a28e219a836b85475
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2017-09-04 19:29:48 +0530

    Create 10.c

---
+++ 10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    long long n;
+    int c = 0;
+
+    printf("Enter an integer: ");
+    scanf("%lld", &n);
+
+    while(n != 0)
+    {
+        
+        n= n/10;
+        ++c;
+    }
+
+    printf("Number of digits: %d", c);
+}
