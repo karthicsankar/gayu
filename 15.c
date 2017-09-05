b1c739d3ae3c6ced0f8f1d3a9e07abbb42f3dac9
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2017-09-05 16:40:44 +0530

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int i;
+ int b;
+ 
+ 
+ cout<<"please enter num"<<endl;
+cin>>i;
+cout<<"enter number"<<endl;
+cin>>b;
+while(i<b){
+ 
+ if(i%2==0)
+ {
+ cout<<"even no. ="<<i<<endl;
+ 
+ }
+ i=i+1;
+}
+}
