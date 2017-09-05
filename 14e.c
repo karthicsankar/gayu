c58c866e685b44754e0cc3dae4e1f70d464f1e01
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2017-09-05 16:28:35 +0530

    Create 14e.c

---
+++ 14e.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int l, h, a, s;
+    printf("Enter  the two numbers: ");
+    scanf("%d %d", &l, &h);
+
+    printf("Prime numbers between %d and %d are: ", l, h);
+
+    while (l< h)
+    {
+        s = 0;
+
+        for(a = 2; a<= l/2; ++a)
+        {
+            if(l % a== 0)
+            {
+                s = 1;
+                break;
+            }
+        }
+
+        if (s== 0)
+            printf("%d ", l);
+
+        ++l;
+    }
+
+    return 0;
+}
