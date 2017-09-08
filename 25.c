482290bbc6bc7b0ed9a33a24a0c122118acfdacf
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2017-09-08 16:44:48 +0530

    Create 25.c

---
+++ 25.c
@@ -0,0 +1,15 @@
+public class JavaProgram
+{
+    public static void main(String args[])
+    {
+	
+        String cha;
+        int a;
+        for(a=1; a<=255; a++)
+        {
+            ch = new Character((char)a).toString();
+            System.out.print(a+ " -> " + cha + "\t");
+        }
+		
+    }
+}
