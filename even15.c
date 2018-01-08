ae64fc9bc391f358e8d53b56d23f0b933ec5f7c6
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2018-01-08 11:09:25 +0000

    Create even15.c

---
+++ even15.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+ int a;
+ int b;
+ printf("please enter number");
+ scanf("%d",a);
+ printf("enter the another number");
+ scanf("%d",b
+
+while(a<b){
+ 
+ if(a%2==0)
+ {
+ printf("even no is %d",a);
+ }
+ a=a+1;
+}
+}
